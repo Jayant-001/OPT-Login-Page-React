db701dc658f0a9da6009b8a154d3879332780cd3
Jayant-001 <jayant.gupta.dln@gmail.com>
2024-01-19 23:49:30 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
     const [phoneNumber, setPhoneNumber] = useState("");
+    // show otp input page if number is filled
     const [showOtpInput, setshowOtpInput] = useState(false);
-    const handleSubmit = (e) => {
-        e.preventDefault();
 
+    // length of otp fields
+    const length = 4;
 
+    const handleNumberSubmit = () => {
+        // validate phone number
+
+        // if number is valid show OTP input fileds
         setshowOtpInput(true);
+    };
+
+    const handleOtpSubmit = (otp) => {
+        // handle OTP validation
+        console.log(otp);
     };
 
     return (
@@ -15,23 +25,34 @@ function App() {
             <h1>Login page</h1>
 
             {showOtpInput ? (
-                <OtpInputPage phoneNumber={phoneNumber} />
+                // to make OptInputPage resuable pass length as parameter
+                <OtpInputPage
+                    handleSubmit={handleOtpSubmit}
+                    phoneNumber={phoneNumber}
+                    length={length}
+                />
             ) : (
                 <NumberInputPage
                     phoneNumber={phoneNumber}
                     setPhoneNumber={setPhoneNumber}
-                    handleSubmit={handleSubmit}
+                    handleSubmit={handleNumberSubmit}
                 />
             )}
         </>
     );
 }
 
+// Component to take phone number input
 const NumberInputPage = ({ phoneNumber, setPhoneNumber, handleSubmit }) => {
     return (
         <>
             <h2>Enter your phone number</h2>
-            <form onSubmit={handleSubmit}>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit();
+                }}
+            >
                 <label htmlFor="number"></label>
                 <input
                     type="number"
@@ -46,15 +67,93 @@ const NumberInputPage = ({ phoneNumber, setPhoneNumber, handleSubmit }) => {
     );
 };
 
-const OtpInputPage = ({phoneNumber}) => {
+const OtpInputPage = ({ phoneNumber, length, handleSubmit }) => {
+    // length define the number of fileds in OTP
+    // create an opt array to store individual digit
+    const [otp, setOtp] = useState(new Array(length).fill(""));
+
+    // reference of all input fields
+    const inputRefs = useRef([]);
+
+    // handle input change in otp fields
+    const handleChange = (event, idx) => {
+        let value = event.target.value; // grab the current input value
+
+        if (isNaN(value)) return; // input must be a number
+
+        const updatedOtp = [...otp];
+        // allow only one digit input || take only last digit of the input
+        updatedOtp[idx] = value.substring(value.length - 1);
+
+        setOtp(updatedOtp);
+
+        // trigger submit function
+        // `setOtp` is asynchronous thats why here we will use `updatedOtp`
+        const combineOtp = updatedOtp.join(""); // convert otp array into a string
+        // submit the OTP if all input fields are filled
+        if (combineOtp.length === 4) {
+            handleSubmit(combineOtp);
+        }
+
+        // move to next empty input field if current field is filled
+        if (value && idx < length - 1 && inputRefs.current[idx + 1]) {
+            inputRefs.current[updatedOtp.indexOf("")].focus();
+        }
+    };
+
+    useEffect(() => {
+        // check if input inputRefs are initialise
+        if (inputRefs.current[0]) {
+            // on component load => focus on the 1st input field
+            inputRefs.current[0].focus();
+        }
+    }, []);
+
+    // handle click on input field
+    const handleClick = (idx) => {
+        // move the input cursor at last
+        inputRefs.current[idx].setSelectionRange(1, 1);
+
+        // check if previous field is empty
+        // move focus to the first empty field
+        if (idx > 0 && !otp[idx - 1]) {
+            inputRefs.current[otp.indexOf("")].focus();
+        }
+    };
+
+    const handleKeyDown = (event, idx) => {
+        // move focus to previous input field on pressing Backspace
+        if (
+            event.key === "Backspace" &&
+            !otp[idx] && // if current field is empty
+            idx > 0 &&
+            inputRefs.current[idx - 1]
+        ) {
+            inputRefs.current[idx - 1].focus();
+        }
+    };
+
     return (
         <>
-        <h3>Opt sent to {phoneNumber}</h3>
-            <form>
-                <input type="number" name="" className="opt-input-box" />
-                <input type="number" name="" className="opt-input-box" />
-                <input type="number" name="" className="opt-input-box" />
-                <input type="number" name="" className="opt-input-box" />
+            <h3>Opt sent to {phoneNumber}</h3>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    // auto submit when all fields are filled
+                }}
+            >
+                {otp.map((value, idx) => (
+                    <input
+                        type="text"
+                        key={idx}
+                        ref={(input) => (inputRefs.current[idx] = input)} // set the reference
+                        value={value}
+                        onChange={(e) => handleChange(e, idx)}
+                        onClick={() => handleClick(idx)}
+                        onKeyDown={(e) => handleKeyDown(e, idx)}
+                        className="opt-input-box"
+                    />
+                ))}
                 <br />
                 <input
                     style={{ margin: "auto", border: "1px solid blue" }}
