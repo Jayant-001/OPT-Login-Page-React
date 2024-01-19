084a61791ac436b15897995805e52e719c88caa2
Jayant-001 <jayant.gupta.dln@gmail.com>
2024-01-19 22:07:59 +0530

    UI page completeg

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [showOtpInput, setshowOtpInput] = useState(false);
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+        setshowOtpInput(true);
+    };
+
+    return (
+        <>
+            <h1>Login page</h1>
+
+            {showOtpInput ? (
+                <OtpInputPage phoneNumber={phoneNumber} />
+            ) : (
+                <NumberInputPage
+                    phoneNumber={phoneNumber}
+                    setPhoneNumber={setPhoneNumber}
+                    handleSubmit={handleSubmit}
+                />
+            )}
+        </>
+    );
 }
 
-export default App
+const NumberInputPage = ({ phoneNumber, setPhoneNumber, handleSubmit }) => {
+    return (
+        <>
+            <h2>Enter your phone number</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="number"></label>
+                <input
+                    type="number"
+                    name="number"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    id="number"
+                />
+                <input type="submit" value="Continue" />
+            </form>
+        </>
+    );
+};
+
+const OtpInputPage = ({phoneNumber}) => {
+    return (
+        <>
+        <h3>Opt sent to {phoneNumber}</h3>
+            <form>
+                <input type="number" name="" className="opt-input-box" />
+                <input type="number" name="" className="opt-input-box" />
+                <input type="number" name="" className="opt-input-box" />
+                <input type="number" name="" className="opt-input-box" />
+                <br />
+                <input
+                    style={{ margin: "auto", border: "1px solid blue" }}
+                    type="submit"
+                    value="Submit"
+                />
+            </form>
+        </>
+    );
+};
+
+export default App;
